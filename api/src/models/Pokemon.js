93f472ab359c7f1938e0ea629d13774781c97b03
JL Acosta_RyokuMaxell <ryoku.maxell11@gmail.com>
2023-08-21 18:30:26 +0000

    Create Pokémon models and Type models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,60 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define(
+    "pokemon",
+    {
+      
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+   
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
   });
 };
